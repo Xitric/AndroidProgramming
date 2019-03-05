e29457ee885231e300d76f40a7519cfe97167943
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2019-03-05 20:32:03 +0100

    Wizard app, exercise 2 and 3

--- WizardApp/app/src/main/java/com/xitric/wizardapp/MainActivity.java
+++ WizardApp/app/src/main/java/com/xitric/wizardapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.xitric.wizardapp;
 
 import android.content.Intent;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i(getClass().getName(), "On create called");
     }
 
     public void nextButtonPressed(View view) {
@@ -20,5 +23,53 @@ public class MainActivity extends AppCompatActivity {
         String name = editText.getText().toString();
         intent.putExtra(Extras.EXTRA_NAME, name);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(getClass().getName(), "On start called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(getClass().getName(), "On resume called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(getClass().getName(), "On pause called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(getClass().getName(), "On stop called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(getClass().getName(), "On restart called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(getClass().getName(), "On destroy called");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.i(getClass().getName(), "On save instance state called");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.i(getClass().getName(), "On restore instance state called");
     }
 }

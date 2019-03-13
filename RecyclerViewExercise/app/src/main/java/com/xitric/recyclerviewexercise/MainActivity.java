568b54b22b0cd1b0189473b86bfa82b4e92cbe82
Kasper Davidsen <KasperSchultzDavidsen@gmail.com>
2019-03-13 19:34:24 +0100

    Recycler view exercise

---
+++ RecyclerViewExercise/app/src/main/java/com/xitric/recyclerviewexercise/MainActivity.java
@@ -0,0 +1,42 @@
+package com.xitric.recyclerviewexercise;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainActivity extends AppCompatActivity implements RandomNumberAdapter.RandomNumberRemovalListener {
+
+    private RecyclerView recycler;
+    private RandomNumberAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recycler = findViewById(R.id.randomNumberRecycler);
+        recycler.setHasFixedSize(true);
+        recycler.setLayoutManager(new GridLayoutManager(this, 2));
+        adapter  =new RandomNumberAdapter(1000, 10000, this);
+        recycler.setAdapter(adapter);
+    }
+
+    @Override
+    public void numberRemoved(final int position, final int value) {
+        String message = "Deleted number " + value + " at position " + position;
+        Snackbar snack = Snackbar.make(findViewById(R.id.content), message, Snackbar.LENGTH_LONG);
+        snack.setAction("Undo", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter.insert(value, position);
+            }
+        });
+        snack.show();
+    }
+}
---
+++ RecyclerViewExercise/app/src/main/java/com/xitric/recyclerviewexercise/RandomNumberAdapter.java
@@ -0,0 +1,72 @@
+package com.xitric.recyclerviewexercise;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumberAdapter extends RecyclerView.Adapter<RandomNumberAdapter.RandomNumberViewHolder> {
+
+    private List<Integer> randomNumbers;
+    private RandomNumberRemovalListener removalListener;
+
+    public RandomNumberAdapter(int count, int max, RandomNumberRemovalListener listener) {
+        randomNumbers = new ArrayList<>(count);
+        for (int i = 0; i < count; i++) {
+            randomNumbers.add((int) (Math.random() * max));
+        }
+        this.removalListener = listener;
+    }
+
+    public void insert(int value, int position) {
+        randomNumbers.add(position, value);
+        notifyItemInserted(position);
+    }
+
+    @NonNull
+    @Override
+    public RandomNumberViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View cellView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.random_number_cell, viewGroup,false);
+        return new RandomNumberViewHolder(cellView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RandomNumberViewHolder randomNumberViewHolder, int i) {
+        randomNumberViewHolder.textView.setText(String.valueOf(randomNumbers.get(i)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return randomNumbers.size();
+    }
+
+    public class RandomNumberViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textView;
+
+        public RandomNumberViewHolder(@NonNull View view) {
+            super(view);
+            this.textView = view.findViewById(R.id.randomNumberLabel);
+            textView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int position = getAdapterPosition();
+                    int value = randomNumbers.remove(position);
+                    notifyItemRemoved(position);
+
+                    removalListener.numberRemoved(position, value);
+                }
+            });
+        }
+    }
+
+    public interface RandomNumberRemovalListener {
+        void numberRemoved(int position, int value);
+    }
+}
